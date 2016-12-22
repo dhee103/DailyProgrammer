185bcbd46aaafac79a99e3e2359f3941064777b0
dhee <dsg115@ic.ac.uk>
2016-12-22 21:39:17 +0000

    Updated and simplified 12DaysOfChristmas

--- 12DaysOfChristmas.c
+++ 12DaysOfChristmas.c
@@ -1,66 +1,28 @@
 #include<stdio.h>
-#include<string.h>
-#include<stdlib.h>
-
-char* concat(const char* s1, const char* s2);
-
-char* getDay(int index);
-
-char* getGifts(int index);
 
 int main() {
   for (size_t i = 1; i <= 12; i++) {
-    char* day = getDay(i);
-    char* gifts = getGifts(i);
-    printf("On the %s day of christmas my true love sent to me: \n %s\n" , day, gifts);
+    char* day[] = {"first", "second", "third", "fourth", "fifth", "sixth",
+                  "seventh", "eighth", "ninth", "tenth", "eleventh", "twelfth"};
+    printf("On the %s day of christmas my true love sent to me:\n" , day[i-1]) ;
+    switch (i) {
+      case 12: puts("Twelve Drummers Drumming");
+      case 11: puts("Eleven Pipers Piping");
+      case 10: puts("Ten Lords a Leaping");
+      case 9:  puts("Nine Ladies Dancing");
+      case 8:  puts("Eight Maids a Milking");
+      case 7:  puts("Seven Swans a Swimming");
+      case 6:  puts("Six Goose a Laying");
+      case 5:  puts("Five Gold Rings");
+      case 4:  puts("Four Calling Birds");
+      case 3:  puts("Three French Hens");
+      case 2:  puts("Two Turtle Doves");
+      case 1:
+        if (i > 1) {
+          printf("and ");
+        }
+        puts("a Partridge in a Pear Tree\n");
+    }
   }
   return 0;
-}
-
-char* concat(const char* s1, const char* s2) {
-  char* result = malloc(strlen(s1) + strlen(s2) + 1);
-  strcpy(result, s1);
-  strcat(result, s2);
-  return result;
-}
-
-char* getDay(int index) {
-  switch (index) {
-    case 1: return "first";
-    case 2: return "second";
-    case 3: return "third";
-    case 4: return "fourth";
-    case 5: return "fifth";
-    case 6: return "sixth";
-    case 7: return "seventh";
-    case 8: return "eighth";
-    case 9: return "ninth";
-    case 10: return "tenth";
-    case 11: return "eleventh";
-    case 12: return "twelfth";
-    default: return "";
-  }
-}
-
-char* getGifts(int index) {
-  char* gifts = "";
-  switch (index) {
-    case 12: gifts = concat(gifts, "Twelve Drummers Drumming\n");
-    case 11: gifts = concat(gifts, "Eleven Pipers Piping\n");
-    case 10: gifts = concat(gifts, "Ten Lords a Leaping\n");
-    case 9: gifts = concat(gifts, "Nine Ladies Dancing\n");
-    case 8: gifts = concat(gifts, "Eight Maids a Milking\n");
-    case 7: gifts = concat(gifts, "Seven Swans a Swimming\n");
-    case 6: gifts = concat(gifts, "Six Goose a Laying\n");
-    case 5: gifts = concat(gifts, "Five Gold Rings\n");
-    case 4: gifts = concat(gifts, "Four Calling Birds\n");
-    case 3: gifts = concat(gifts, "Three French Hens\n");
-    case 2: gifts = concat(gifts, "Two Turtle Doves\n");
-    case 1:
-      if (index > 1) {
-        gifts = concat(gifts, "and ");
-      }
-      gifts = concat(gifts, "a Partridge in a Pear Tree\n");
-  }
-  return gifts;
 }
